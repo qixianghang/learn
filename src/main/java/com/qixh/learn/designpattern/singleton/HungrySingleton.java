edff2adbc699d4bfc665a88f94e796e3ec1bb16d
qixh <qixianghang@163.com>
2019-05-16 17:33:25 +0800

    注解*变小问题

--- src/main/java/com/qixh/learn/designpattern/singleton/HungrySingleton.java
+++ src/main/java/com/qixh/learn/designpattern/singleton/HungrySingleton.java
@@ -5,13 +5,12 @@ package com.qixh.learn.designpattern.singleton;
  * @author qixh
  * @date 2019年5月16日
  *
- * 饿汉式：
- *   直接创建实例对象，不管你是否需要这个对象
- *   
- *   （1）构造器私有化
- *   （2）自行创建，并且用静态变量保存
- *   （3）向外提供这个实例
- *   （4）强调这是一个单例，我们可以用final修改
+ **饿汉式：
+ **  直接创建实例对象，不管你是否需要这个对象   
+ ** （1）构造器私有化
+ ** （2）自行创建，并且用静态变量保存
+ ** （3）向外提供这个实例
+ ** （4）强调这是一个单例，我们可以用final修改
  */
 public class HungrySingleton {
 	private static final HungrySingleton INSTANCE = new HungrySingleton();
